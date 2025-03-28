8c686c0e0da2fc3525a61a914858efc825edc3fa
Adrian Espindola <103772656+AdrianMEsp@users.noreply.github.com>
2025-03-27 22:39:16 -0300

    Delete ExpenseTrackerApp

--- ExpenseTrackerApp/src/main/java/com/ExpenseTrackerApp/ExpenseTrackerApp/repository/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ExpenseTrackerApp.ExpenseTrackerApp.repository;
-
-import com.ExpenseTrackerApp.ExpenseTrackerApp.model.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CategoryRepository extends JpaRepository<Category,Long> {
-}
